a0a27317e9cc910046a65fd1f97796a06a6199e4
angela <atakux@users.noreply.github.com>
2022-02-16 09:58:20 -0800

    Update list_manipulation.py

--- list_manipulation.py
+++ list_manipulation.py
@@ -14,6 +14,7 @@ import lists
 
 sports_teams = [lists.football_teams, lists.baseball_teams, lists.basketball_teams]
 
+
 #swaps out hot dogs for bratwurst in menu using listcomp
 menu = [food if food != "hot dogs" else "bratwurst" for food in lists.school_lunches]
 print(menu)
@@ -24,34 +25,12 @@ for questions, answers in zip(lists.questions, lists.answers):
     print("What is your {0}? My {0} is {1}".format(questions, answers))
 
 
-#grab all the NY teams
-newYork = "New York"
-NY = []
-for i in sports_teams[0]:
-    if newYork in i:
-        NY.append(i)
-for i in sports_teams[1]:
-    if newYork in i:
-        NY.append(i)
-for i in sports_teams[2]:
-    if newYork in i:
-        NY.append(i)
+
+#grabs all the new york teams
+NY = [ny for nyTeams in sports_teams for ny in nyTeams if "New York" in ny]
 print(NY)
 
 
-
-#grab all the LA teams
-losAngeles = "Los Angeles"
-LA = []
-for i in sports_teams[0]:
-    if losAngeles in i:
-        LA.append(i)
-for i in sports_teams[1]:
-    if losAngeles in i:
-        LA.append(i)
-for i in sports_teams[2]:
-    if losAngeles in i:
-        LA.append(i)
+#grabs all the los angeles teams
+LA = [la for laTeams in sports_teams for la in laTeams if "Los Angeles" in la]
 print(LA)
-
-
